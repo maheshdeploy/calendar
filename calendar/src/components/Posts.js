73f35df6e49078ccfd0beef3bca7879fbdbc73aa
paul idemudia <osaspofficial@gmail.com>
2019-04-08 10:40:21 +0100

    wip

--- calendar/src/components/Posts.js
+++ calendar/src/components/Posts.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
   render() {
     const postItems = this.state.posts.map(post => (
       <div key={post.id}>
